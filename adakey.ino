4546dd9987e6ba537f578c3af366be88593690b0
Colton Begert <begert@ualberta.ca>
2017-12-10 00:10:07 -0700

    test

--- adakey.ino
+++ adakey.ino
@@ -1,13 +1,27 @@
 #include <Adafruit_NeoPixel.h>
 
+#ifdef __AVR__
+    #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
+
+
 void setup()
 {
-    Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
     strip.begin();
     strip.show(); // Initialize all pixels to 'off'
+    // strip.setBrightness(1);
+    // red = 0;
+    // pos = 0;
+    
+
 }
 
 void loop()
 {
-	
+    return;
 }
